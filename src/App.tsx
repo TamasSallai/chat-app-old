f65e28e8d62aa9fe5cff59b9d9ee4429a3315447
TamasSallai <43635741+TamasSallai@users.noreply.github.com>
2023-02-04 21:05:45 +0100

    Revert "created routing with where context gets value based on url param id"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { useEffect, useMemo } from 'react'
-import { Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
 import LoginPage from './pages/LoginPage/LoginPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import ChatPage from './pages/ChatPage/ChatPage'
 import ProtectedRoute from './ProtectRoute'
-import { useChatContext } from './context/chat'
 
 function App() {
-  const match = useMatch('/chat/:id')
-  const matchMemo = useMemo(() => match?.params.id, [match])
-  const [, dispatch] = useChatContext()
-
-  useEffect(() => {
-    if (matchMemo) {
-      dispatch({ type: 'SELECT_CHAT', payload: matchMemo })
-    }
-  }, [matchMemo, dispatch])
-
   return (
     <div>
       <Navigation />
@@ -26,7 +14,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route
-          path="/chat/*"
+          path="/chat"
           element={
             <ProtectedRoute>
               <ChatPage />
