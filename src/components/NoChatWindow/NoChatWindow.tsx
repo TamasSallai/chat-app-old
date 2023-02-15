3995a224f5aa7ff5da70abb12b9f30ea66f4363f
TamasSallai <tamas.sallai.hu@gmail.com>
2023-02-15 14:38:32 +0100

    added 404 page when navigating to a non exsisting id

---
+++ src/components/NoChatWindow/NoChatWindow.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './NoChatWindow.css'
+
+const NoChatWindow = () => {
+  return <div className="no-chat-window">404 Can't find chat</div>
+}
+
+export default NoChatWindow
